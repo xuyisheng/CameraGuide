69745b24de3800bf5042c998bb4f098be46877d7
xuyisheng <xuyisheng@hujiang.com>
2015-06-25 22:25:57 +0800

    init

---
+++ app/src/main/java/com/xys/cameraguide/MainActivity.java
@@ -0,0 +1,68 @@
+package com.xys.cameraguide;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+
+    private static int CAMERA_CODE1 = 1;
+    private String mFilePath = "";
+    private ImageView mImageViewShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageViewShow = (ImageView) findViewById(R.id.iv_show);
+        String tempPath = Environment.getExternalStorageDirectory().getPath();
+        mFilePath = tempPath + "/" + "test1.png";
+    }
+
+    public void camera1(View view) {
+        Intent intent = new Intent(
+                MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri photoUri = Uri.fromFile(
+                new File(mFilePath));
+        intent.putExtra(
+                MediaStore.EXTRA_OUTPUT,
+                photoUri);
+        startActivityForResult(intent, CAMERA_CODE1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == CAMERA_CODE1) {
+            FileInputStream fis = null;
+            Bitmap bitmap = null;
+            try {
+                fis = new FileInputStream(mFilePath);
+                bitmap = BitmapFactory.decodeStream(fis);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            mImageViewShow.setImageBitmap(bitmap);
+        }
+    }
+}
